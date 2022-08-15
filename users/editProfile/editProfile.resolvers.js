cca50b801afbb1508afcbb0f4c9d50242c20d99b
Mason3144 <lts890303@gmail.com>
2022-08-16 01:26:37 +0900

    Mutation editProfile without authentication

--- users/editProfile/editProfile.resolvers.js
+++ users/editProfile/editProfile.resolvers.js
@@ -1,5 +1,34 @@
+import bcrypt from "bcrypt";
+import client from "../../client";
+
 export default {
   Mutation: {
-    editProfile: async () => {},
+    editProfile: async (
+      _,
+      { firstName, lastName, username, email, password: newPassword }
+    ) => {
+      try {
+        let hash = null;
+        if (newPassword) {
+          hash = await bcrypt.hash(newPassword, 10);
+        }
+        const updatedUser = await client.user.update({
+          where: { id: 4 },
+          data: {
+            firstName,
+            lastName,
+            username,
+            email,
+            ...(hash && { password: hash }),
+          },
+        });
+        if (!updatedUser.id) {
+          return { ok: false, error: "Could not update" };
+        }
+        return { ok: true };
+      } catch (error) {
+        return { ok: false, error };
+      }
+    },
   },
 };
