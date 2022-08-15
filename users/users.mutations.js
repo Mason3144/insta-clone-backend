762d125868ed080de90ef8fa98a23bd68a8f5269
Mason3144 <lts890303@gmail.com>
2022-08-16 00:19:07 +0900

    Mutation login part

--- users/users.mutations.js
+++ users/users.mutations.js
@@ -1,5 +1,6 @@
 import bcrypt from "bcrypt";
 import client from "../client";
+import jwt from "jsonwebtoken";
 
 export default {
   Mutation: {
@@ -24,6 +25,18 @@ export default {
       } catch (e) {
         return e;
       }
+    },
+    login: async (_, { username, password }) => {
+      const user = await client.user.findFirst({ where: { username } });
+      if (!user) {
+        return { ok: false, error: "User not found." };
+      }
+      const checkPassword = await bcrypt.compare(password, user.password);
+      if (!checkPassword) {
+        return { ok: false, error: "Incorrect password." };
+      }
+      const token = await jwt.sign({ id: user.id }, process.env.SECRET_KEY);
+      return { ok: true, token };
     },
   },
 };
